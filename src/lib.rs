66b72a3a79c6f9e502180db31ff7599e7502e2d2
kolena <kolena@avast.com>
2017-12-16 23:57:18 +0100

    Using rdedup-lib in list endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ extern crate time;
 extern crate serde_json;
 extern crate multimap;
 extern crate rocket;
+extern crate rdedup_lib as rdedup;
 
 use multimap::MultiMap;
 use failure::Error;
@@ -19,88 +20,84 @@ use std::ops::Deref;
 use std::str;
 use rocket::data::Data;
 
+use rdedup::Repo;
+
 #[derive(Debug, Fail)]
 #[fail(display = "Could not get exit code of ext program")]
 struct MissingExitCode;
+//
+//pub fn save(repo: Repo, pc_id: &str, orig_file_name: &str, data: Data) -> Result<(), Error> {
+//    let current_time = SystemTime::now()
+//        .duration_since(UNIX_EPOCH)?;
+//
+//    let time_stamp = current_time.as_secs() * 1000 + u64::from(current_time.subsec_nanos()) / 1000;
+//
+//    debug!("Current time: {}", time_stamp);
+//
+//    let file_name_final = to_final_name(pc_id, orig_file_name, time_stamp);
+//
+//    debug!("Final name: {}", file_name_final);
+//
+//    let mut child = Command::new("rdedup")
+//        .stdin(Stdio::piped())
+//        .arg("--dir")
+//        .arg(repo_dir)
+//        .arg("store")
+//        .arg(file_name_final)
+//        .spawn()?;
+//
+//
+//    let stdin = child.stdin.take();
+//
+//    data.stream_to(&mut stdin.unwrap())?;
+//
+//    let output = child.wait_with_output()?;
+//
+//    let exit_code = output.status.code().ok_or(MissingExitCode)?;
+//    debug!("Exit code: {}", exit_code);
+//
+//    if exit_code == 0 {
+//        let split = str::from_utf8(&output.stdout)?
+//            .trim()
+//            .lines();
+//
+//        debug!("Output: {:?}", split.collect::<Vec<_>>());
+//
+//        Ok(())
+//    } else {
+//        let output = String::from_utf8(output.stderr)?;
+//
+//        warn!("Exit code {}, stderr: {:?}", exit_code, output);
+//
+//        // TODO Err
+//        Ok(())
+//    }
+//}
+//
+//pub fn load(repo: Repo, pc_id: &str, orig_file_name: &str, time_stamp: u64) -> Result<ChildStdout, Error> {
+//    let file_name_final = to_final_name(pc_id, orig_file_name, time_stamp);
+//
+//    debug!("Requested name: {}", file_name_final);
+//
+//    Command::new("rdedup")
+//        .env("RDEDUP_PASSPHRASE", "jenda")
+//        .arg("--dir")
+//        .arg(repo_dir)
+//        .arg("load")
+//        .arg(file_name_final)
+//        .stdout(Stdio::piped())
+//        .spawn()
+//        .map_err(Error::from)
+//        .map(|stdout| {
+//            stdout.stdout.unwrap()
+//        })
+//}
 
-pub fn save(repo_dir: &str, pc_id: &str, orig_file_name: &str, data: Data) -> Result<(), Error> {
-    let current_time = SystemTime::now()
-        .duration_since(UNIX_EPOCH)?;
-
-    let time_stamp = current_time.as_secs() * 1000 + u64::from(current_time.subsec_nanos()) / 1000;
-
-    debug!("Current time: {}", time_stamp);
-
-    let file_name_final = to_final_name(pc_id, orig_file_name, time_stamp);
-
-    debug!("Final name: {}", file_name_final);
-
-    let mut child = Command::new("rdedup")
-        .stdin(Stdio::piped())
-        .arg("--dir")
-        .arg(repo_dir)
-        .arg("store")
-        .arg(file_name_final)
-        .spawn()?;
-
-
-    let stdin = child.stdin.take();
-
-    data.stream_to(&mut stdin.unwrap())?;
-
-    let output = child.wait_with_output()?;
-
-    let exit_code = output.status.code().ok_or(MissingExitCode)?;
-    debug!("Exit code: {}", exit_code);
-
-    if exit_code == 0 {
-        let split = str::from_utf8(&output.stdout)?
-            .trim()
-            .lines();
-
-        debug!("Output: {:?}", split.collect::<Vec<_>>());
-
-        Ok(())
-    } else {
-        let output = String::from_utf8(output.stderr)?;
-
-        warn!("Exit code {}, stderr: {:?}", exit_code, output);
-
-        // TODO Err
-        Ok(())
-    }
-}
-
-pub fn load(repo_dir: &str, pc_id: &str, orig_file_name: &str, time_stamp: u64) -> Result<ChildStdout, Error> {
-    let file_name_final = to_final_name(pc_id, orig_file_name, time_stamp);
-
-    debug!("Requested name: {}", file_name_final);
-
-    Command::new("rdedup")
-        .env("RDEDUP_PASSPHRASE", "jenda")
-        .arg("--dir")
-        .arg(repo_dir)
-        .arg("load")
-        .arg(file_name_final)
-        .stdout(Stdio::piped())
-        .spawn()
-        .map_err(Error::from)
-        .map(|stdout| {
-            stdout.stdout.unwrap()
-        })
-}
-
-pub fn list(repo_dir: &str, pc_id: &str) -> Result<String, Error> {
-    Command::new("rdedup")
-        .arg("--dir")
-        .arg(repo_dir)
-        .arg("ls")
-        .output()
+pub fn list(repo: &Repo, pc_id: &str) -> Result<String, Error> {
+    repo.list_names()
         .map_err(Error::from)
         .and_then(|output| {
-            let out = str::from_utf8(&output.stdout)?;
-
-            let data = out.trim().lines().filter_map(|l: &str| {
+            let data = output.into_iter().filter_map(|l: String| {
                 let parts = l.split('#').collect::<Vec<&str>>();
 
                 match (parts.get(0), parts.get(1), parts.get(2).map(|num| num.parse::<u64>())) {
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,20 @@ extern crate rbackup;
 extern crate config;
 extern crate rocket;
 #[macro_use]
-extern crate log;
+extern crate slog;
+extern crate slog_async;
+extern crate slog_term;
+extern crate url;
+extern crate rdedup_lib as rdedup;
 
 use failure::Error;
 use rocket::Data;
 use rocket::response::Stream;
 use rocket::State;
 use std::process::ChildStdout;
+
+use std::path::Path;
+use rdedup::Repo;
 
 #[derive(FromForm)]
 struct UploadMetadata {
@@ -34,47 +41,60 @@ struct ListMetadata {
 
 #[get("/list?<metadata>")]
 fn list(config: State<AppConfig>, metadata: ListMetadata) -> Result<String, Error> {
-    rbackup::list(&config.repo_dir, &metadata.pc_id)
+    rbackup::list(&config.repo, &metadata.pc_id)
 }
 
-#[get("/download?<metadata>")]
-fn download(config: State<AppConfig>, metadata: DownloadMetadata) -> Result<Stream<ChildStdout>, Error> {
-    rbackup::load(&config.repo_dir, &metadata.pc_id, &metadata.orig_file_name, metadata.time_stamp)
-        .map(|stdout| {
-            Stream::from(
-                stdout
-            )
-        })
-}
+//
+//#[get("/download?<metadata>")]
+//fn download(config: State<AppConfig>, metadata: DownloadMetadata) -> Result<Stream<ChildStdout>, Error> {
+//    rbackup::load(&config.repo_dir, &metadata.pc_id, &metadata.orig_file_name, metadata.time_stamp)
+//        .map(|stdout| {
+//            Stream::from(
+//                stdout
+//            )
+//        })
+//}
+//
+//#[post("/upload?<metadata>", format = "application/octet-stream", data = "<data>")]
+//fn upload(config: State<AppConfig>, data: Data, metadata: UploadMetadata) -> &'static str {
+//    match rbackup::save(&config.repo_dir, &metadata.pc_id, &metadata.orig_file_name, data) {
+//        Ok(()) => {
+//            "ok"
+//        }
+//        Err(e) => {
+//            warn!(config.logger, "{}", e);
+//            "FAIL"
+//        }
+//    }
+//}
 
-#[post("/upload?<metadata>", format = "application/octet-stream", data = "<data>")]
-fn upload(config: State<AppConfig>, data: Data, metadata: UploadMetadata) -> &'static str {
-    match rbackup::save(&config.repo_dir, &metadata.pc_id, &metadata.orig_file_name, data) {
-        Ok(()) => {
-            "ok"
-        }
-        Err(e) => {
-            warn!("{}", e);
-            "FAIL"
-        }
-    }
-}
 
 struct AppConfig {
-    repo_dir: String
+    repo: Repo,
+    logger: slog::Logger
 }
 
+
 fn main() {
+//    let decorator = slog_term::PlainDecorator::new(std::io::stdout());
+//    let drain = slog_term::CompactFormat::new(decorator).build().fuse();
+//    let drain = slog_async::Async::new(drain).build().fuse();
+
+    let logger = slog::Logger::root(slog::Discard, o!());
+
     let mut config = config::Config::default();
     let config = config.merge(config::File::with_name("Settings")).unwrap();
 
+    let repo_dir = &config.get_str("repo").expect("Could not extract repo path from config");
+
     let config = AppConfig {
-        repo_dir: config.get_str("repo").expect("Could not extract repo path from config")
+        repo: Repo::open(&Path::new(repo_dir), logger.clone()).expect("Could not open repo"),
+        logger
     };
 
     rocket::ignite()
-        .mount("/", routes![upload])
-        .mount("/", routes![download])
+//        .mount("/", routes![upload])
+//        .mount("/", routes![download])
         .mount("/", routes![list])
         .manage(config)
         .launch();
