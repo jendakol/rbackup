f2ee9c44d39b911388b76762d8fd3d2226e634eb
kolena <kolena@avast.com>
2018-07-24 22:29:12 +0200

    improved tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -6,15 +6,29 @@ function upload() {
 
     sha=$(sha256sum "$file_name" | awk '{ print $1 }')
 
-    curl -sS --header "Content-Type: multipart/form-data" -H "RBackup-Session-Pass: ${session_id}" \
+    result=$(curl -sS --header "Content-Type: multipart/form-data" -H "RBackup-Session-Pass: ${session_id}" \
         -F file=@"${file_name}" -F file-hash="${sha}" \
-        -X POST "http://localhost:3369/upload?file_name=config"
+        -X POST "http://localhost:3369/upload?file_name=config")
+
+    if  [[ ${result} == Failure* ]];
+    then
+        echo ${result}
+        exit 1
+    fi
 }
 
 function list_files() {
     session_id=$1
 
-    curl -sS -H "RBackup-Session-Pass: ${session_id}" -X GET "http://localhost:3369/list/files/?"
+    result=$(curl -sS -H "RBackup-Session-Pass: ${session_id}" -X GET "http://localhost:3369/list/files/?")
+
+echo $result
+
+    if  [[ ${result} == Failure* ]];
+    then
+        echo ${result}
+        exit 1
+    fi
 }
 
 function assert() {
@@ -22,7 +36,7 @@ function assert() {
     actual=$2
     hint=$3
 
-    test "${expected}" = "${actual}" || echo "${hint}"
+    test "${expected}" = "${actual}" || (echo "${hint}" && exit 1)
 }
 
 echo -e "Running tests:\n"
@@ -30,7 +44,7 @@ echo -e "Running tests:\n"
 curl -sS "http://localhost:3369/account/register?username=rbackup&password=rbackup" > /dev/null \
  && session_id=$(curl -sS "http://localhost:3369/account/login?device_id=docker-tests&username=rbackup&password=rbackup") \
  && echo -e "SessionID: ${session_id} \n" \
- && upload ${session_id} config.toml > /dev/null \
+ && upload ${session_id} config.toml \
  && second_response=$(upload ${session_id} config.toml) \
  && list_response_sha=$(list_files ${session_id} | jq '.[] | {original_name: .original_name, versions: [.versions[] | { version: .version, hash: .hash, size: .size }] }' | sha256sum | awk '{ print $1 }') \
  && assert "aae65f1df784e1ef2c9e12da0eaf78429044d32f1d5ed961d5e1ed4436bd9a89" ${list_response_sha} "List response content was different" \
